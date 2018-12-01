29161fd16afefca2f15683e6968c5b60dc43d59e
wangkun <kennywhite@163.com>
2018-12-01 14:29:27 +0800

    update file

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -1,5 +1,7 @@
 package com.example.demo.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,9 +10,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping
 public class HelloController {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(HelloController.class);
+
 
     @GetMapping(value = "/hello")
     public String hello() {
+        LOGGER.info("my first spring-boot-docker application");
         return "my first spring-boot-docker application";
     }
 }
