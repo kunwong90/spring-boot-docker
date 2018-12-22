04927224547e07dc872e4dd0b07cec71ae504d20
wangkun <kennywhite@163.com>
2018-12-22 10:41:15 +0800

    增加dockerHost配置

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -2,6 +2,7 @@ package com.example.demo.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,15 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(HelloController.class);
+
+
+    @Value(value = "${envName}")
+    private String envName;
+
+    @GetMapping(value = "/envName")
+    private String envName() {
+        return envName;
+    }
 
 
     @GetMapping(value = "/hello")
