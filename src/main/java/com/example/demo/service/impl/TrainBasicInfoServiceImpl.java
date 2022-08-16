e394d40c34baf3707444c37d9bd99f9d25f65677
wangkun2 <kennywhite@163.com>
2022-08-16 10:11:33 +0800

    add

--- src/main/java/com/example/demo/service/impl/TrainBasicInfoServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/TrainBasicInfoServiceImpl.java
@@ -8,6 +8,7 @@ import com.example.demo.service.QueryTrainStationName;
 import com.example.demo.trainprice.utils.h12306.core.CoreZZCX;
 import com.example.demo.trainprice.utils.h12306.pojo.PJInfo;
 import com.example.demo.trainprice.utils.h12306.pojo.TrainInfo;
+import com.example.demo.trainprice.utils.h12306.pojo.YunXingInfo;
 import com.example.demo.util.JacksonJsonUtils;
 import okhttp3.OkHttpClient;
 import org.apache.commons.collections4.CollectionUtils;
@@ -178,89 +179,98 @@ public class TrainBasicInfoServiceImpl implements ITrainBasicInfoService {
                         try {
                             for (String depName : subList) {
                                 for (String destName : stationNameList) {
-                                    TrainBasicInfo trainBasicInfo = new TrainBasicInfo();
-                                    trainBasicInfo.setDepartureStationName(depName);
-                                    trainBasicInfo.setDestStationName(destName);
-                                    LocalDate localDate = LocalDate.now();
-                                    Instant instant = localDate.atStartOfDay().atZone(ZoneId.systemDefault()).toInstant();
-                                    trainBasicInfo.setDepartureDate(Date.from(instant));
-                                    long start = System.currentTimeMillis();
-                                    String date = new SimpleDateFormat("yyyyMMdd").format(trainBasicInfo.getDepartureDate());
-                                    List<TrainInfo> trainInfoList = coreZZCX.query(trainBasicInfo.getDepartureStationName(), trainBasicInfo.getDestStationName(), Integer.parseInt(date));
-                                    LOGGER.info("查询耗时 = {}", (System.currentTimeMillis() - start));
-                                    if (CollectionUtils.isNotEmpty(trainInfoList)) {
-                                        for (TrainInfo trainInfo : trainInfoList) {
-                                            TrainBasicInfo query = new TrainBasicInfo();
-                                            query.setTrainNo(trainInfo.getCc());
-                                            query.setDepartureStationName(trainInfo.getFz());
-                                            query.setDestStationName(trainInfo.getDz());
-                                            try {
-                                                query.setDepartureDate(DateUtils.parseDate(date, "yyyyMMdd"));
-                                            } catch (Exception e) {
-                                                LOGGER.error("日期转换异常", e);
-                                            }
-                                            TrainBasicInfo result = trainBasicInfoMapper.selectOne(query);
-                                            PJInfo pjInfo = trainInfo.getPj();
-                                            TrainBasicInfo trainBasicInfo1 = new TrainBasicInfo();
-                                            trainBasicInfo1.setTrainNo(trainInfo.getCc());
-                                            trainBasicInfo1.setStartStationName(trainInfo.getSfz());
-                                            trainBasicInfo1.setEndStationName(trainInfo.getZdz());
-                                            trainBasicInfo1.setDepartureStationName(trainInfo.getFz());
-                                            trainBasicInfo1.setDestStationName(trainInfo.getDz());
-                                            trainBasicInfo1.setDepartureDate(trainBasicInfo.getDepartureDate());
-                                            trainBasicInfo1.setDistance(trainInfo.getLc());
-                                            trainBasicInfo1.setYdz(pjInfo.getYdz());
-                                            trainBasicInfo1.setEdz(pjInfo.getEdz());
-                                            trainBasicInfo1.setSwz(pjInfo.getSwz());
-                                            trainBasicInfo1.setTdz(pjInfo.getTdz());
-                                            trainBasicInfo1.setRz(pjInfo.getRz());
-                                            trainBasicInfo1.setYz(pjInfo.getYz());
-                                            // 查询不到
-                                            trainBasicInfo1.setGjrws(pjInfo.BLANK_PRICE);
-                                            // 查询不到
-                                            trainBasicInfo1.setGjrwx(pjInfo.BLANK_PRICE);
-                                            trainBasicInfo1.setDws(pjInfo.getDw());
-                                            trainBasicInfo1.setDwx(pjInfo.BLANK_PRICE);
-                                            if (StringUtils.startsWith(trainBasicInfo.getTrainNo(), "D")) {
-                                                trainBasicInfo1.setYdws(pjInfo.getRws());
-                                                trainBasicInfo1.setYdwx(pjInfo.getRwx());
-                                                trainBasicInfo1.setEdws(pjInfo.getYws());
-                                                trainBasicInfo1.setEdwz(pjInfo.getYwz());
-                                                trainBasicInfo1.setEdwx(pjInfo.getYwx());
+                                    for (int dateIndex = 0; dateIndex < 15; dateIndex++) {
+                                        TrainBasicInfo trainBasicInfo = new TrainBasicInfo();
+                                        trainBasicInfo.setDepartureStationName(depName);
+                                        trainBasicInfo.setDestStationName(destName);
+                                        LocalDate localDate = LocalDate.now().plusDays(dateIndex);
+                                        Instant instant = localDate.atStartOfDay().atZone(ZoneId.systemDefault()).toInstant();
+                                        trainBasicInfo.setDepartureDate(Date.from(instant));
+                                        long start = System.currentTimeMillis();
+                                        String date = new SimpleDateFormat("yyyyMMdd").format(trainBasicInfo.getDepartureDate());
+                                        List<TrainInfo> trainInfoList = coreZZCX.query(trainBasicInfo.getDepartureStationName(), trainBasicInfo.getDestStationName(), Integer.parseInt(date));
+                                        LOGGER.info("查询耗时 = {}", (System.currentTimeMillis() - start));
+                                        if (CollectionUtils.isNotEmpty(trainInfoList)) {
+                                            //LOGGER.info("查询参数 = {},结果 = {}", JacksonJsonUtils.toJson(trainBasicInfo), JacksonJsonUtils.toJson(trainInfoList));
+                                            for (TrainInfo trainInfo : trainInfoList) {
+                                                YunXingInfo yunXingInfo = trainInfo.getBasicYunXingInfo();
+                                                Date jsrq = new SimpleDateFormat("yyyyMMdd").parse(String.valueOf(yunXingInfo.getJsrq()));
+                                                if (jsrq.before(trainBasicInfo.getDepartureDate())) {
+                                                    LOGGER.warn("结束日期 {} 大于发车时间 {}", yunXingInfo.getJsrq(), date);
+                                                    continue;
+                                                }
+                                                TrainBasicInfo query = new TrainBasicInfo();
+                                                query.setTrainNo(trainInfo.getCc());
+                                                query.setDepartureStationName(trainInfo.getFz());
+                                                query.setDestStationName(trainInfo.getDz());
+                                                try {
+                                                    query.setDepartureDate(DateUtils.parseDate(date, "yyyyMMdd"));
+                                                } catch (Exception e) {
+                                                    LOGGER.error("日期转换异常", e);
+                                                }
+                                                TrainBasicInfo result = trainBasicInfoMapper.selectOne(query);
+                                                PJInfo pjInfo = trainInfo.getPj();
+                                                TrainBasicInfo trainBasicInfo1 = new TrainBasicInfo();
+                                                trainBasicInfo1.setTrainNo(trainInfo.getCc());
+                                                trainBasicInfo1.setStartStationName(trainInfo.getSfz());
+                                                trainBasicInfo1.setEndStationName(trainInfo.getZdz());
+                                                trainBasicInfo1.setDepartureStationName(trainInfo.getFz());
+                                                trainBasicInfo1.setDestStationName(trainInfo.getDz());
+                                                trainBasicInfo1.setDepartureDate(trainBasicInfo.getDepartureDate());
+                                                trainBasicInfo1.setDistance(trainInfo.getLc());
+                                                trainBasicInfo1.setYdz(pjInfo.getYdz());
+                                                trainBasicInfo1.setEdz(pjInfo.getEdz());
+                                                trainBasicInfo1.setSwz(pjInfo.getSwz());
+                                                trainBasicInfo1.setTdz(pjInfo.getTdz());
+                                                trainBasicInfo1.setRz(pjInfo.getRz());
+                                                trainBasicInfo1.setYz(pjInfo.getYz());
+                                                // 查询不到
+                                                trainBasicInfo1.setGjrws(pjInfo.BLANK_PRICE);
+                                                // 查询不到
+                                                trainBasicInfo1.setGjrwx(pjInfo.BLANK_PRICE);
+                                                trainBasicInfo1.setDws(pjInfo.getDw());
+                                                trainBasicInfo1.setDwx(pjInfo.BLANK_PRICE);
+                                                if (StringUtils.startsWith(trainBasicInfo.getTrainNo(), "D")) {
+                                                    trainBasicInfo1.setYdws(pjInfo.getRws());
+                                                    trainBasicInfo1.setYdwx(pjInfo.getRwx());
+                                                    trainBasicInfo1.setEdws(pjInfo.getYws());
+                                                    trainBasicInfo1.setEdwz(pjInfo.getYwz());
+                                                    trainBasicInfo1.setEdwx(pjInfo.getYwx());
 
-                                                trainBasicInfo1.setRws(pjInfo.BLANK_PRICE);
-                                                trainBasicInfo1.setRwx(pjInfo.BLANK_PRICE);
-                                                trainBasicInfo1.setYws(pjInfo.BLANK_PRICE);
-                                                trainBasicInfo1.setYwz(pjInfo.BLANK_PRICE);
-                                                trainBasicInfo1.setYwx(pjInfo.BLANK_PRICE);
-                                            } else {
-                                                trainBasicInfo1.setYdws(pjInfo.BLANK_PRICE);
-                                                trainBasicInfo1.setYdwx(pjInfo.BLANK_PRICE);
-                                                trainBasicInfo1.setEdws(pjInfo.BLANK_PRICE);
-                                                trainBasicInfo1.setEdwz(pjInfo.BLANK_PRICE);
-                                                trainBasicInfo1.setEdwx(pjInfo.BLANK_PRICE);
+                                                    trainBasicInfo1.setRws(pjInfo.BLANK_PRICE);
+                                                    trainBasicInfo1.setRwx(pjInfo.BLANK_PRICE);
+                                                    trainBasicInfo1.setYws(pjInfo.BLANK_PRICE);
+                                                    trainBasicInfo1.setYwz(pjInfo.BLANK_PRICE);
+                                                    trainBasicInfo1.setYwx(pjInfo.BLANK_PRICE);
+                                                } else {
+                                                    trainBasicInfo1.setYdws(pjInfo.BLANK_PRICE);
+                                                    trainBasicInfo1.setYdwx(pjInfo.BLANK_PRICE);
+                                                    trainBasicInfo1.setEdws(pjInfo.BLANK_PRICE);
+                                                    trainBasicInfo1.setEdwz(pjInfo.BLANK_PRICE);
+                                                    trainBasicInfo1.setEdwx(pjInfo.BLANK_PRICE);
 
-                                                trainBasicInfo1.setRws(pjInfo.getRws());
-                                                trainBasicInfo1.setRwx(pjInfo.getRwx());
-                                                trainBasicInfo1.setYws(pjInfo.getYws());
-                                                trainBasicInfo1.setYwz(pjInfo.getYwz());
-                                                trainBasicInfo1.setYwx(pjInfo.getYwx());
-                                            }
-                                            trainBasicInfo1.setWz(pjInfo.getWz());
-                                            trainBasicInfo1.setQt(pjInfo.getQt());
-                                            trainBasicInfo1.setUpdateTime(new Date());
+                                                    trainBasicInfo1.setRws(pjInfo.getRws());
+                                                    trainBasicInfo1.setRwx(pjInfo.getRwx());
+                                                    trainBasicInfo1.setYws(pjInfo.getYws());
+                                                    trainBasicInfo1.setYwz(pjInfo.getYwz());
+                                                    trainBasicInfo1.setYwx(pjInfo.getYwx());
+                                                }
+                                                trainBasicInfo1.setWz(pjInfo.getWz());
+                                                trainBasicInfo1.setQt(pjInfo.getQt());
+                                                trainBasicInfo1.setUpdateTime(new Date());
 
-                                            if (result == null) {
-                                                trainBasicInfo1.setAddTime(new Date());
-                                                trainBasicInfoMapper.insert(trainBasicInfo1);
-                                            } else {
-                                                trainBasicInfo1.setId(result.getId());
-                                                LOGGER.warn("更新数据,查询入参 = {}", JacksonJsonUtils.toJson(query));
-                                                trainBasicInfoMapper.updateById(trainBasicInfo1);
+                                                if (result == null) {
+                                                    trainBasicInfo1.setAddTime(new Date());
+                                                    trainBasicInfoMapper.insert(trainBasicInfo1);
+                                                } else {
+                                                    trainBasicInfo1.setId(result.getId());
+                                                    //LOGGER.warn("更新数据,查询入参 = {}", JacksonJsonUtils.toJson(query));
+                                                    trainBasicInfoMapper.updateById(trainBasicInfo1);
+                                                }
                                             }
+                                        } else {
+                                            LOGGER.info("查询结果为空,参数 = {}", JacksonJsonUtils.toJson(trainBasicInfo));
                                         }
-                                    } else {
-                                        LOGGER.info("查询结果为空,参数 = {}", JacksonJsonUtils.toJson(trainBasicInfo));
                                     }
                                 }
                             }
@@ -270,7 +280,6 @@ public class TrainBasicInfoServiceImpl implements ITrainBasicInfoService {
 
                     });
                 }
-
             }
         } catch (Exception e) {
             LOGGER.error("异常", e);
