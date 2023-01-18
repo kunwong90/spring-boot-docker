da454a4efe3abde2847ec5d15e07d7572e8f5733
wangkun2 <kennywhite@163.com>
2023-01-18 09:42:53 +0800

    add

---
+++ src/main/java/com/example/demo/controller/MultiUploadControllerTest.java
@@ -0,0 +1,69 @@
+package com.example.demo.controller;
+
+
+import org.apache.http.HttpEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.ContentType;
+import org.apache.http.entity.mime.HttpMultipartMode;
+import org.apache.http.entity.mime.MultipartEntityBuilder;
+import org.apache.http.entity.mime.content.StringBody;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.util.EntityUtils;
+
+import java.io.File;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class MultiUploadControllerTest {
+
+    public static void main(String[] args) {
+        HttpPost postMethod = new HttpPost("http://localhost:8080/multiUpload");
+
+
+        Map<String, String> input = new HashMap<>();
+        input.put("key", "123");
+        input.put("channel", "OTA");
+        input.put("airline", "HU");
+
+        MultipartEntityBuilder builder = MultipartEntityBuilder.create();
+        try {
+            CloseableHttpClient httpClient = HttpClientBuilder.create().build();
+            builder.setMode(HttpMultipartMode.BROWSER_COMPATIBLE);
+            for (Map.Entry<String, String> entry : input.entrySet()) {
+                String key = entry.getKey();
+                StringBody stringBody = new StringBody(entry.getValue(), ContentType.MULTIPART_FORM_DATA);
+                builder.addPart(key, stringBody);
+            }
+
+            List<File> files = new ArrayList<>();
+            files.add(new File("F:\\1.png"));
+            files.add(new File("F:\\c.txt"));
+            files.add(new File("F:\\aaa.zip"));
+            for (File file : files) {
+                builder.addBinaryBody("file", Files.newInputStream(file.toPath()), ContentType.create("multipart/form-data"), file.getName());
+            }
+            builder.setMode(HttpMultipartMode.BROWSER_COMPATIBLE);
+            builder.setCharset(StandardCharsets.UTF_8);
+            postMethod.setEntity(builder.build());
+            HttpEntity reqEntity = builder.build();
+            postMethod.setEntity(reqEntity);
+            CloseableHttpResponse closeableHttpResponse = httpClient.execute(postMethod);
+
+            HttpEntity entity = closeableHttpResponse.getEntity();
+            if (entity != null) {
+                String result = EntityUtils.toString(entity, "UTF-8");
+                System.out.println(result);
+            }
+        } catch (Exception e) {
+
+        }
+
+
+    }
+}
--- src/main/java/com/example/demo/controller/UploadController.java
+++ src/main/java/com/example/demo/controller/UploadController.java
@@ -3,9 +3,9 @@ package com.example.demo.controller;
 
 import com.example.demo.entity.DishonourRefundDto;
 import com.example.demo.util.JacksonJsonUtils;
+import org.apache.commons.collections4.CollectionUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
@@ -14,6 +14,7 @@ import sun.misc.BASE64Decoder;
 import javax.servlet.http.HttpServletRequest;
 import javax.xml.bind.DatatypeConverter;
 import java.io.FileOutputStream;
+import java.util.List;
 
 @Controller
 @RequestMapping
@@ -41,5 +42,29 @@ public class UploadController {
         FileOutputStream out = new FileOutputStream(targetPath);
         out.write(buffer);
         out.close();
+    }
+
+
+    @PostMapping(value = "/multiUpload")
+    @ResponseBody
+    public String multiUpload(DishonourRefundDto request, HttpServletRequest servletRequest, List<MultipartFile> file) {
+        System.out.println(JacksonJsonUtils.toJson(request));
+
+        try {
+            if (CollectionUtils.isNotEmpty(file)) {
+                for (MultipartFile file1 : file) {
+                    String fileSuffix = file1.getOriginalFilename().substring(file1.getOriginalFilename().lastIndexOf("."));
+                    System.out.println("附件名 = " + file1.getOriginalFilename() + " ,附件大小 = " + file1.getSize());
+                    String base64 = DatatypeConverter.printBase64Binary(file1.getBytes());
+                    decoderBase64File(base64, "F:\\test" + System.currentTimeMillis() + fileSuffix);
+                }
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+        return "{\"success\":true}";
     }
 }
